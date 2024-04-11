38e1785a97958bd1443e39fa4a8698e7252c0d26
Fatih Karaoğlu <r05h3k98@gmail.com>
2024-04-11 10:03:27 +0300

    Ask user for numbers till an odd number is entered. If the even numbers are dividable by 4, take their summatin

---
+++ Java101/src/KosulluIfadelerVeKodBloklari/DividerFourSum.java
@@ -0,0 +1,24 @@
+package KosulluIfadelerVeKodBloklari;
+
+import java.util.Scanner;
+
+public class DividerFourSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long sum = 0;
+        boolean isDividable = true;
+
+        while (isDividable) {
+            System.out.print("Sayı giriniz: ");
+            int number = scanner.nextInt();
+            if(number % 2 == 1 || number % 2 == -1) {
+                isDividable = false;
+            } else {
+                if (number % 4 == 0) {
+                    sum += number;
+                }
+            }
+        }
+        System.out.printf("Toplam: %d", sum);
+    }
+}
