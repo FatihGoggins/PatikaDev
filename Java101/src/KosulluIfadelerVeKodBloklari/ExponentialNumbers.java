d0229c6547787c38d71099cf31a8a49b690da8ae
Fatih Karaoğlu <r05h3k98@gmail.com>
2024-04-13 09:44:03 +0300

    Exponential numbers

---
+++ Java101/src/KosulluIfadelerVeKodBloklari/ExponentialNumbers.java
@@ -0,0 +1,21 @@
+package KosulluIfadelerVeKodBloklari;
+
+import java.util.Scanner;
+
+public class ExponentialNumbers {
+    public static void main(String[] args) {
+        int number, power;
+        long result = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Üssü alınacak sayı: ");
+        number = scanner.nextInt();
+        System.out.print("Üs alınacak sayı: ");
+        power = scanner.nextInt();
+
+        for (int i = 0; i< power; i++) {
+            result *= number;
+        }
+
+        System.out.printf("Sonuç: %d", result);
+    }
+}
