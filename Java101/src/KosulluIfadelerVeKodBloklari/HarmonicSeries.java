e9467edf2e8487e3629ffe661c959c2b14c8b0e0
Fatih Karaoğlu <r05h3k98@gmail.com>
2024-04-13 10:38:34 +0300

    Harmonic Series Sum

---
+++ Java101/src/KosulluIfadelerVeKodBloklari/HarmonicSeries.java
@@ -0,0 +1,17 @@
+package KosulluIfadelerVeKodBloklari;
+
+import java.util.Scanner;
+
+public class HarmonicSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("N sayısını giriniz: ");
+        long n = scanner.nextLong();
+        double seriesSum = 0;
+
+        for (long i = 1; i<=n; i++) {
+            seriesSum += 1/(double)i;
+        }
+        System.out.printf("Harmonik Seri toplamı = %f", seriesSum);
+    }
+}
